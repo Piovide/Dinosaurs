1e58460d14b197aa83f8a1f990e0ba71836898c2
Piovide <122434180+Piovide@users.noreply.github.com>
2026-02-21 01:01:46 +0100

    asd

--- vite.config.js
+++ vite.config.js
@@ -20,10 +20,16 @@ export default defineConfig({
         }),
     ],
     build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
+    outDir: 'public/build',
+    emptyOutDir: true,
+    rollupOptions: {
+        output: {
+            entryFileNames: 'assets/[name]-[hash].js',
+            chunkFileNames: 'assets/[name]-[hash].js',
+            assetFileNames: 'assets/[name]-[hash].[ext]',
+        },
     },
+},
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
